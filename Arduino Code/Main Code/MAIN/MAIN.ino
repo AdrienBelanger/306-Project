83f3af16a6fbc9094d2ac028ae8ce2040b21f20b
AdrienBelanger <adribelanger@gmail.com>
2024-12-01 15:36:54 -0500

    bpm correct with qol! But low bpm at first tap

--- Arduino Code/Main Code/MAIN/MAIN.ino
+++ Arduino Code/Main Code/MAIN/MAIN.ino
@@ -9,21 +9,20 @@ int pedal_one = 0;
 int count_beats = 0;
 
 String lastMessage = "";
-
 String message = "";
 
 unsigned long last_time_of_hit = 0;
-
+unsigned long last_beat_time = 0;
 unsigned long last_clock_time = 0; // For MIDI Clock timing
+unsigned long square_display_start_time = 0;
+
 int interval = 0;
-
 int bpm = 0;
-
 int bpm_at_prev = 0;
-
 int last_bpm = 0;
-
 int cur_state = 0;
+
+bool square_displayed = false; // Tracks if the square is currently displayed
 
 void setup() {
   lcd.begin(16, 2);
@@ -34,6 +33,7 @@ void setup() {
   pinMode(22, INPUT_PULLUP); // rotary switch
   pinMode(21, INPUT_PULLUP); // rotary switch
   pinMode(20, INPUT_PULLUP); // rotary switch
+
   Serial.begin(9600);
 
   last_time_of_hit = millis();
@@ -41,45 +41,28 @@ void setup() {
 }
 
 void loop() {
-  // --------------- INPUT ---------------
-  // --------- PEDALS INPUT --------
+  // PEDALS INPUT
   int pedal1State = digitalRead(6);
-
-  if (pedal1State == LOW) {
-    pedal_one = 0;
-  } else {
-    pedal_one = 1;
-  }
+  pedal_one = (pedal1State == LOW) ? 0 : 1;
 
   int pedal2State = digitalRead(19);
+  pedal_two = (pedal2State == LOW) ? 0 : 1;
 
-  if (pedal2State == LOW) {
-    pedal_two = 0;
-  } else {
-    pedal_two = 1;
-  }
-
-  // --------- ROTARY SWITCH INPUT --------
-
-  // Make sure states are 0 when off and 1 when on
+  // ROTARY SWITCH INPUT
   int state0 = 1 - digitalRead(22); // right
   int state1 = 1 - digitalRead(21); // center
-  int state2 = 1 - digitalRead(20);  // left
+  int state2 = 1 - digitalRead(20); // left
 
-  // --------------- MAIN CODE ---------------
-  // PRINT THE CURRENT STATE AND UPDATE
-
+  // MAIN CODE
   if (state0 == 1 && state1 == 0 && state2 == 0) {
     message = "    WELCOME";
     lcd.setCursor(0, 1);
     lcd.print("                ");
     cur_state = 0;
-  }
-  else if (state0 == 0 && state1 == 1 && state2 == 0) { 
+  } else if (state0 == 0 && state1 == 1 && state2 == 0) { 
     message = "  FOLLOW MODE";
     cur_state = 1;
-  }
-  else if (state0 == 0 && state1 == 0 && state2 == 1) { 
+  } else if (state0 == 0 && state1 == 0 && state2 == 1) { 
     message = "   HOLD MODE";
     cur_state = 2;
   }
@@ -97,7 +80,6 @@ void loop() {
   }
 
   if (cur_state != 0 && pedal_one == 1 && pedal_two == 1) { // if welcome, ignore the BPM
-    // Give time to debounce. This, however, limits the max taps per second to 5, giving a max bpm of 300
     interval = millis() - last_time_of_hit;
     if (interval > 200) {
       last_time_of_hit = millis();
@@ -113,7 +95,6 @@ void loop() {
   }
 
   if (cur_state == 2 && pedal_one == 0 && pedal_two == 0 && bpm != last_bpm) {
-    // Go back to bpm before we started tapping the bpm
     bpm = last_bpm;
     lcd.setCursor(0, 1);
     lcd.print("                ");
@@ -123,13 +104,40 @@ void loop() {
     lcd.print(bpm);
   }
 
-  // Send MIDI Clock messages
+  unsigned long current_time = millis();
+
+  // MIDI Clock sending
   if (bpm > 0) {
-    unsigned long current_time = millis();
     int clock_interval = 60000 / (bpm * 24); // Calculate interval between MIDI Clock pulses
     if (current_time - last_clock_time >= clock_interval) {
       last_clock_time = current_time;
       usbMIDI.sendRealTime(usbMIDI.Clock); // Send MIDI Clock pulse
+    }
+
+    // Beat counting and square display
+    int beat_interval = 60000 / bpm; // Calculate time per beat in milliseconds
+    if (current_time - last_beat_time >= beat_interval) {
+      last_beat_time = current_time;
+      count_beats += 1;
+      Serial.print("Beats at this BPM: ");
+      Serial.println(count_beats);
+
+      usbMIDI.sendNoteOn(60, 127, 1); // Note C4, Velocity 127, Channel 1
+      if (bpm < 180) {
+        square_display_start_time = current_time;
+        square_displayed = true;
+        lcd.setCursor(15, 1);
+        lcd.print("\xFF"); // Display a square character
+      }
+      delay(10);
+      usbMIDI.sendNoteOff(60, 0, 1); // Note C4, Velocity 0, Channel 1
+    }
+
+    // Remove square after 200ms
+    if (square_displayed && current_time - square_display_start_time >= 200) {
+      lcd.setCursor(15, 1);
+      lcd.print(" "); // Clear the square
+      square_displayed = false;
     }
   }
 
